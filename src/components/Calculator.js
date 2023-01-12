f19a5798041218903a0f4b7640e76a6f7c11837e
gijoe2day <gjuliao32@hotmail.com>
2023-01-12 10:03:10 -0500

    Refactor Calculator class to function component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,65 +1,63 @@
 /* eslint-disable quotes, no-console */
 
-import React from "react";
+import React, { useState } from "react";
 import BtnNumbers from "./BtnNumbers";
 import BtnOperator from "./BtnOperator";
 import Zero from "./Zero";
 import calculate from "../logic/calculate";
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0, next: null, operation: null,
-    };
-  }
+const Calculator = () => {
+  const [value, setValue] = useState({
+    total: 0, next: null, operation: null,
+  });
 
-  handleClick = (e) => {
-    this.setState((state) => calculate(state, e.target.innerText));
+  const { total, next, operation } = value;
+
+  const handleClick = (e) => {
+    setValue((state) => calculate(state, e.target.innerText));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
+  return (
 
-    return (
-      <section className="calculator mt-5 text-center">
-        <div className="output d-flex justify-content-end bg-secondary text-light">
-          <p className="my-auto p-2">
-            {total}
-            {operation}
-            {next}
-          </p>
-        </div>
-        <div className="d-flex">
-          <BtnNumbers handleClick={this.handleClick} item="AC" />
-          <BtnNumbers handleClick={this.handleClick} item="+/-" />
-          <BtnNumbers handleClick={this.handleClick} item="%" />
-          <BtnOperator handleClick={this.handleClick} item="÷" />
-        </div>
-        <div className="d-flex">
-          <BtnNumbers handleClick={this.handleClick} item="7" />
-          <BtnNumbers handleClick={this.handleClick} item="8" />
-          <BtnNumbers handleClick={this.handleClick} item="9" />
-          <BtnOperator handleClick={this.handleClick} item="x" />
-        </div>
-        <div className="d-flex">
-          <BtnNumbers handleClick={this.handleClick} item="4" />
-          <BtnNumbers handleClick={this.handleClick} item="5" />
-          <BtnNumbers handleClick={this.handleClick} item="6" />
-          <BtnOperator handleClick={this.handleClick} item="-" />
-        </div>
-        <div className="d-flex">
-          <BtnNumbers handleClick={this.handleClick} item="1" />
-          <BtnNumbers handleClick={this.handleClick} item="2" />
-          <BtnNumbers handleClick={this.handleClick} item="3" />
-          <BtnOperator handleClick={this.handleClick} item="+" />
-        </div>
-        <div className="d-flex">
-          <Zero handleClick={this.handleClick} item="0" />
-          <BtnNumbers handleClick={this.handleClick} item="." />
-          <BtnOperator handleClick={this.handleClick} item="=" />
-        </div>
-      </section>
-    );
-  }
-}
+    <section className="calculator mt-5 text-center">
+      <div className="output d-flex justify-content-end bg-secondary text-light">
+        <p className="my-auto p-2">
+          {total}
+          {operation}
+          {next}
+        </p>
+      </div>
+      <div className="d-flex">
+        <BtnNumbers handleClick={handleClick} item="AC" />
+        <BtnNumbers handleClick={handleClick} item="+/-" />
+        <BtnNumbers handleClick={handleClick} item="%" />
+        <BtnOperator handleClick={handleClick} item="÷" />
+      </div>
+      <div className="d-flex">
+        <BtnNumbers handleClick={handleClick} item="7" />
+        <BtnNumbers handleClick={handleClick} item="8" />
+        <BtnNumbers handleClick={handleClick} item="9" />
+        <BtnOperator handleClick={handleClick} item="x" />
+      </div>
+      <div className="d-flex">
+        <BtnNumbers handleClick={handleClick} item="4" />
+        <BtnNumbers handleClick={handleClick} item="5" />
+        <BtnNumbers handleClick={handleClick} item="6" />
+        <BtnOperator handleClick={handleClick} item="-" />
+      </div>
+      <div className="d-flex">
+        <BtnNumbers handleClick={handleClick} item="1" />
+        <BtnNumbers handleClick={handleClick} item="2" />
+        <BtnNumbers handleClick={handleClick} item="3" />
+        <BtnOperator handleClick={handleClick} item="+" />
+      </div>
+      <div className="d-flex">
+        <Zero handleClick={handleClick} item="0" />
+        <BtnNumbers handleClick={handleClick} item="." />
+        <BtnOperator handleClick={handleClick} item="=" />
+      </div>
+    </section>
+  );
+};
+
+export default Calculator;
